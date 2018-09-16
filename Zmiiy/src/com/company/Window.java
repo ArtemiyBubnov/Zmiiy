8b09517381524391bda436700951cf95bc6e09af
REAL-BS\student12 <arta.9696@bk.ru>
2018-09-16 12:09:17 +0400

    New window

---
+++ Zmiiy/src/com/company/Window.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+    Window(){
+        setVisible(true);
+        setBounds(190, 80, 800,600);
+        setTitle("SnakeGame");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+}
+
